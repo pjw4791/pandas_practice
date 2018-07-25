bf6c1ca544bb50055d322506fcf6e448c7b1d26e
Seoyoung Park <pjw4791@gmail.com>
2018-07-25 19:09:00 +0900

    2018/7/25(Wed) Finish

--- zipline_practice.py
+++ zipline_practice.py
@@ -4,7 +4,7 @@ yf.pdr_override()
 
 import datetime
 import matplotlib.pyplot as plt
-from zipline.api import order, symbol
+from zipline.api import order, symbol, order_target, record
 from zipline.algorithm import TradingAlgorithm
 
 start = datetime.datetime(2010, 1, 1)
@@ -15,9 +15,16 @@ data = data[['Adj Close']]
 data.columns = ['AAPL']
 data = data.tz_localize('UTC')
 
+# 변수 i와 sym을 context라는 네임스페이스에 추가
 def initialize(context):
-    pass
+    # pass
+    # i라는 변수는 거래일 수를 계산하는 데 사용되는 변수이고
+    context.i = 0
 
+    # sym은 참조할 데이터에 대한 심볼을 저장하는데 사용됩니다.
+    context.sym = symbol('AAPL')
+
+# 거래일마다 1주씩 매수하는 알고리즘
 def handle_data(context, data):
     order(symbol('AAPL'), 1)
 
